5adad07352dea2d75cc5e5bf2b96933b40fd0627
eatplaynap <eatplaynap329@gmail.com>
2022-05-06 13:56:48 +0900

    部屋情報を30件登録できるseedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do |n|
+  Room.create!(
+    name: "room #{n+1}",
+    place: "#{n+1}F",
+    number: n,
+    terms_of_use: "Great room"
+  )
+end
