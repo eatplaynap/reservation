95e5aee4f02d0937f872ef8743668d6948c5aed4
eatplaynap <eatplaynap329@gmail.com>
2022-05-06 11:34:32 +0900

    rails g migration add_terms_of_use_to_rooms terms_of_use:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_050341) do
+ActiveRecord::Schema.define(version: 2022_05_06_022652) do
 
   create_table "entries", force: :cascade do |t|
     t.string "user_name"
@@ -30,15 +30,7 @@ ActiveRecord::Schema.define(version: 2022_05_05_050341) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "email"
-    t.date "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "terms_of_use"
   end
 
 end
