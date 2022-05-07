ded73eadcfe555d6088e206ce3653c30b15d1f54
eatplaynap <eatplaynap329@gmail.com>
2022-05-07 17:18:01 +0900

    理解度チェックのバリデーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
-end
+  validates :name, presence: true, length: { maximum: 30 }, format: { with: /.*#\d{2}\z/ }
+  validates :place, presence: true,  inclusion: { in: ['東京', '大阪', '福岡', '札幌', '仙台', '名古屋', '金沢'] }
+  validates :number, presence: true, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
