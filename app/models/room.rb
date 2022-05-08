880dbad4c1855e218cdd2346d95f4815734d9be1
eatplaynap <eatplaynap329@gmail.com>
2022-05-08 14:37:10 +0900

    理解度チェックの独自バリデーションヘルパー実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,21 @@
 class Room < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30 }, format: { with: /.*#\d{2}\z/ }
+  validates :name, presence: true, length: { maximum: 30 }
   validates :place, presence: true,  inclusion: { in: ['東京', '大阪', '福岡', '札幌', '仙台', '名古屋', '金沢'] }
-  validates :number, presence: true, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
+  validates :number, presence: true
+  validate :number_check
+  validate :name_check
+
+  private
+  def name_check
+    name_pattern = /.*#\d{2}\z/
+    unless name_pattern.match(self.name)
+      errors.add(:name, '会議室名が正しくありません')
+    end
+  end
+
+  def number_check
+    unless self.number % 5 == 0
+      errors.add(:number, '人数は5の倍数を入力ください')
+    end
+  end
+end
