686ca1e6475a3fa411a75f1336bcc89eccb68cab
eatplaynap <eatplaynap329@gmail.com>
2022-05-09 20:23:47 +0900

    理解度チェックのスコープ実装

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,6 @@
 class Entry < ApplicationRecord
+  scope :least_entries, -> (date) {
+    where( "reserved_date >= ? and reserved_date <= ?",
+          date.to_date - 7.days, date.to_date + 7.days )
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 30.times do |n|
   Room.create!(
     name: "room #{n+1}",
     place: "#{n+1}F",
     number: n,
     terms_of_use: "Great room"
+  )
+end
+
+20.times do |n|
+  Entry.create!(
+    reserved_date: Date.today - 10 + n.days
   )
 end
