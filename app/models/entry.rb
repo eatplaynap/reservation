b0f8d417bb92cafc82a3d1c36c6c4da2667e4074
eatplaynap <eatplaynap329@gmail.com>
2022-05-10 11:45:23 +0900

    理解度チェック(p.289)のRoomモデルとEntryモデルに1対多の親子関係設定

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ApplicationRecord
+  belongs_to :room
+
   scope :least_entries, -> (date) {
     where( "reserved_date >= ? and reserved_date <= ?",
           date.to_date - 7.days, date.to_date + 7.days )
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
+  has_many :entries
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :place, presence: true,  inclusion: { in: ['東京', '大阪', '福岡', '札幌', '仙台', '名古屋', '金沢'] }
   validates :number, presence: true
