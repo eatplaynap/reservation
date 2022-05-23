dd0289e0680d765d085731be031e6422db2acd87
eatplaynap <eatplaynap329@gmail.com>
2022-05-23 11:00:17 +0900

    理解度チェック8.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'top#index'
   resources :rooms
-  resources :entries, only: [:new, :create, :destroy, :index]
+  resources :entries, only: [:new, :create, :destroy, :index], path: :rentals
 end
