d9b7c0469e8cd248c1f1c60217ac30522977eee4
eatplaynap <eatplaynap329@gmail.com>
2022-05-27 11:06:58 +0900

    理解度チェック9.1

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -25,6 +25,7 @@ class RoomsController < ApplicationController
 
     respond_to do |format|
       if @room.save
+        session[:room] = @room.name
         format.html { redirect_to room_url(@room), notice: "Room was successfully created." }
         format.json { render :show, status: :created, location: @room }
       else
